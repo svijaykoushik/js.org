3393cc414ac887f3d1b44bb947cbf1fd8d2c44de
Vijay Koushik, S. <svijaykoushik@hotmail.com>
2026-05-20 22:33:15 +0530

    Add i8e10 to cnames_active.js
    Add i8e10 project landing page to cname

--- cnames_active.js
+++ cnames_active.js
@@ -1504,6 +1504,7 @@ var cnames_active = {
   "hyperform": "hyperform.github.io",
   "hyperhtml-styleguide": "albertosantini.github.io/hyperhtml-styleguide", // noCF
   "hypixel": "hypixelts.netlify.app",
+  "i8e10": "svijaykoushik.github.io/i8e10",
   "i18n-collector": "gmaxlev.github.io/i18n-collector",
   "i18n4v": "shibukawa.github.io/i18n4v",
   "iagrib": "iagrib.github.io",
